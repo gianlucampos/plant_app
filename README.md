91ffa5684f67064a938d3a1bce7e1e5e00be03a6
gianlucampos <gianlucampos97@gmail.com>
2023-08-18 22:00:03 -0300

    Adicionando splashscreen

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # plant_app
 
 A new Flutter project.
+
+Commands:
+
+flutter clean && flutter pub get && dart pub run flutter_native_splash:create
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:plant_app/core/app_colors.dart';
 import 'package:plant_app/pages/welcome_page.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MainApp());
+  _loadApp();
+}
+
+Future _loadApp() async {
+  await Future.delayed(const Duration(seconds: 3));
+  FlutterNativeSplash.remove();
 }
 
 class MainApp extends StatelessWidget {
@@ -18,7 +27,7 @@ class MainApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(
           backgroundColor: AppColors.darkGreen,
           iconTheme: IconThemeData(
-            color: Colors.white, //change your color here
+            color: Colors.white,
           ),
           elevation: 1,
           shape: RoundedRectangleBorder(
